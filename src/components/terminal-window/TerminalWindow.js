6179c6c975bd221367725b8402b30b6df8901fd0
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-06-15 16:52:41 -0400

    finished terminal css

--- src/components/terminal-window/TerminalWindow.js
+++ src/components/terminal-window/TerminalWindow.js
@@ -5,20 +5,19 @@ export default class TerminalWindow extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="menu">
-          <div className="button-flex">
+        <div className="top-bar">
+          <div className="button-container">
             <div className="button red"></div>
             <div className="button yellow"></div>
             <div className="button green"></div>
           </div>
-          <div class="title">
+          <div className="title">
             <a href="https://github.com/Sujeev-Uthayakumar">
-              <h1>
-                <i class="fab fa-github"></i>github.com/Sujeev-Uthayakumar
-              </h1>
+              <h1>github.com/sujeev-uthayakumar</h1>
             </a>
           </div>
         </div>
+        <div id="app"></div>
       </div>
     );
   }
