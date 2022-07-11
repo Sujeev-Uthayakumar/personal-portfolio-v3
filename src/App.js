6fab29ae730e219f4bae7f4dba78b791bfee5d66
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-07-11 09:35:39 -0400

    .gitignore is now working

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import TerminalWindow from "./components/terminal-window/TerminalWindow";
-
-export default class App extends React.Component {
-  render() {
-    return <TerminalWindow />;
-  }
-}
