e28ab46d1adc6b4d7142b3761063595509abcb4e
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-10-07 00:54:28 -0400

    setup mainpage

--- src/components/home/MainPage.js
+++ src/components/home/MainPage.js
@@ -3,6 +3,6 @@ import "./MainPage.css";
 
 export default class MainPage extends React.Component {
   render() {
-    return <div className="test">dawda</div>;
+    return <div>website goes here</div>;
   }
 }
