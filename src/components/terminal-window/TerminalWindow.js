d6dd6641a55262571840f7d05acdfde9f9481ece
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-10-10 14:22:42 -0400

    refactor container css

--- src/components/terminal-window/TerminalWindow.js
+++ src/components/terminal-window/TerminalWindow.js
@@ -144,7 +144,9 @@ export default class TerminalWindow extends React.Component {
         <React.Fragment>
           <div
             className={
-              !this.state.terminalActive ? "display-none" : "container"
+              !this.state.terminalActive
+                ? "display-none"
+                : "terminal-container container"
             }
             onClick={containerFocus}
           >
