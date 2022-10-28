ee1e0f9f9240c6e7cade50ecab3b0e8eac75a4c9
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-10-28 15:10:32 -0400

    add deployment to google app engine

---
+++ portfolio-backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT);
