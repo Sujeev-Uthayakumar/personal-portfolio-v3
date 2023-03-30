d93cb38f6a42b43b204da3e8b0b065599c14e366
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2023-03-29 23:15:01 -0400

    remove parallex effect and switch to typewriter effect

--- client/src/sections/main/MainSection.js
+++ client/src/sections/main/MainSection.js
@@ -1,25 +1,32 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
+
+import bannerBackground from "../../assets/images/banner-image.avif";
 import "./MainSection.css";
-import stars from "../../assets/images/stars.png";
-import moon from "../../assets/images/moon.png";
-import mountainsBehind from "../../assets/images/mountains_behind.png";
-import mountainsFront from "../../assets/images/mountains_front.png";
 
 const MainSection = () => {
   return (
-    <section>
-      <img src={stars} id="stars" alt="stars"></img>
-      <img src={moon} id="moon" alt="moon"></img>
-      <img
-        src={mountainsBehind}
-        id="mountains_behind"
-        alt="mountains_behind"
-      ></img>
-      <img
-        src={mountainsFront}
-        id="mountains_front"
-        alt="mountains_front"
-      ></img>
+    <section
+      style={{
+        backgroundImage: `url(${bannerBackground})`,
+      }}
+      className="container-fluid background"
+    >
+      <div className="main-section d-flex flex-grow-1 justify-content-center align-items-center">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("Hello World!")
+              .pauseFor(2500)
+              .deleteAll()
+              .start();
+          }}
+          options={{
+            loop: true,
+          }}
+          className="typewriter"
+        />
+      </div>
     </section>
   );
 };
