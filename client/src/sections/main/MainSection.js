ae5163e9e16d2127b6be5cafa982384fb781c1fe
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2023-03-30 06:59:59 -0400

    start work on main section

--- client/src/sections/main/MainSection.js
+++ client/src/sections/main/MainSection.js
@@ -12,20 +12,35 @@ const MainSection = () => {
       }}
       className="container-fluid background"
     >
-      <div className="main-section d-flex flex-grow-1 justify-content-center align-items-center">
-        <Typewriter
-          onInit={(typewriter) => {
-            typewriter
-              .typeString("Hello World!")
-              .pauseFor(2500)
-              .deleteAll()
-              .start();
-          }}
-          options={{
-            loop: true,
-          }}
-          className="typewriter"
-        />
+      <div className="main-section d-flex flex-grow-1  align-items-center">
+        <div className="container">
+          <div className="row d-flex">
+            <div className="col-lg-12">
+              <h1 className="start-text">Hi, my name is </h1>
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString("Sujeev Uthayakumar")
+                    .pauseFor(2500)
+                    .deleteAll()
+                    .start();
+                }}
+                options={{
+                  loop: true,
+                  wrapperClassName: "typewriter",
+                  cursorClassName: "cursor",
+                }}
+              />
+              <h3 className="big-text">I engineer software for the web.</h3>
+              <p className="small-text">
+                I’m a software engineer who's experienced in working on
+                fullstack applicatins. Currently, I'm focused on gaining as much
+                experience as possible working as a Software Engineering Intern
+                at <a href="https://www.celestica.com/">Celestica</a>.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
